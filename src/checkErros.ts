c5b7a9d2f8fc147b9ffa24d2b4e18c75f353e83a
Mikael Rolim de Aquino <60241602+Mikael-R@users.noreply.github.com>
2021-10-30 16:24:33 -0300

    Refactor for best code (#22)
    * docs: add more information in readme
    
    * fix: change typescript lib
    
    * refactor: use switch

--- src/checkErros.ts
+++ src/checkErros.ts
@@ -1,53 +1,37 @@
 import { Preferences } from './types';
 
-interface CheckErros {
-  (preferences: Preferences): void;
-}
-
-const checkErrors: CheckErros = ({ length, initialText = '', useChars }) => {
-  if (length === undefined) {
-    throw new Error('Password length not informed');
-  }
-
-  if (Number.isNaN(Number(length))) {
-    throw new Error('Password length not is number');
-  }
-
-  if (length < 4 || length > 1024) {
-    throw new Error('Invalid password length [min=4|max=1024]');
-  }
-
-  if (typeof initialText !== 'string') {
-    throw new Error('Initial text not is string');
-  }
-
-  if (initialText?.length >= length) {
-    throw new Error(
-      'Initial text cannot be greater than or equal to password length'
-    );
-  }
-
-  if (useChars === undefined) {
-    throw new Error('Characters not informed');
-  }
-
-  if (typeof useChars !== 'object') {
-    throw new Error('Characters not is object');
-  }
-
+const checkErrors = ({ length, initialText = '', useChars }: Preferences) => {
   const usedChars: [string, boolean][] = Object.entries(useChars).filter(
     value => value[1]
   );
 
-  if (!usedChars.length) {
-    throw new Error('Characters not has been selected');
-  }
+  switch (true) {
+    case length === undefined:
+      throw new Error('Password length not informed');
 
-  if (
-    usedChars.length > 1 &&
-    usedChars.indexOf(['pronounceable', true]) !== -1
-  ) {
-    throw new Error('Pronounceable its checked with others chars');
+    case Number.isNaN(Number(length)):
+      throw new Error('Password length not is number');
+
+    case length < 4 || length > 1024:
+      throw new Error('Invalid password length [min=4|max=1024]');
+
+    case typeof initialText !== 'string':
+      throw new Error('Initial text not is string');
+
+    case initialText?.length >= length:
+      throw new Error('Initial text cannot be greater than or equal to password length');
+
+    case useChars === undefined:
+      throw new Error('Characters not informed');
+
+    case typeof useChars !== 'object':
+      throw new Error('Characters not is object');
+
+    case !usedChars.length:
+      throw new Error('Characters not has been selected');
+
+    case usedChars.length > 1 && usedChars.indexOf(['pronounceable', true]) !== -1:
+      throw new Error('Pronounceable its checked with others chars');
   }
 };
 
